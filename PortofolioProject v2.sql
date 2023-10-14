27b56bb166c6874281dc5ece478b2e9d987ce493
AmrAli64 <137122916+AmrAli64@users.noreply.github.com>
2023-10-14 17:53:24 +0300

    Add files via upload

---
+++ PortofolioProject v2.sql
@@ -0,0 +1,8 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[AccumaltiveVaccinations]
+  FROM [PortfolioProject].[dbo].[PercentPopulationVaccinated]
+   
