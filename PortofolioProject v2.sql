8821336b832e04a1826f1d0d07be7d5e8d8fec30
AmrAli64 <137122916+AmrAli64@users.noreply.github.com>
2023-10-15 20:28:02 +0300

    Delete PortofolioProject v2.sql

--- PortofolioProject v2.sql
+++
@@ -1,8 +0,0 @@
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[AccumaltiveVaccinations]
-  FROM [PortfolioProject].[dbo].[PercentPopulationVaccinated]
-   
